3228ec5649a6c11785d99cf349cb5bde68d8b46f
Shweta Vermani <shwetanagpal83@gmail.com>
2023-01-28 11:42:31 -0500

    Assessment

--- java/module-2/week-10/assessment/part_one_sql/queries/02_number_of_books_authored.sql
+++ java/module-2/week-10/assessment/part_one_sql/queries/02_number_of_books_authored.sql
@@ -2,10 +2,11 @@
 -- Order the results by the number of books in descending order (highest first), then by alphabetical order of the author's first name.
 -- Tip: make sure to add a space between the author's first and last names.
 -- (16 rows)
-SELECT  (first_name || ' '|| last_name) AS author, COUNT(book_title) AS num_books
-FROM person p 
-JOIN book b ON p.person_id = b.foreword_by
-JOIN book_author ba ON b.book_id = ba.book_id
-GROUP BY first_name, last_name
-ORDER BY num_books DESC,  p.first_name ;
+
+SELECT  person.first_name || ' '|| person. last_name AS author, COUNT(book_title) AS num_books
+FROM book 
+JOIN person ON book.foreword_by = person.person_id
+JOIN book_author ON person.person_id = book_author.author_id
+GROUP BY first_name, last_name, book_title
+ORDER BY num_books DESC, person.first_name;
 
