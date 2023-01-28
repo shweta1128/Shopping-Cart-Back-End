52b41b1686b3e72636dc4c966266bd9bbbe7fcb1
Shweta Vermani <shwetanagpal83@gmail.com>
2023-01-28 10:20:33 -0500

    Assessment

--- java/module-2/week-10/assessment/part_two_dao/src/main/java/com/techelevator/dao/JdbcBookDao.java
+++ java/module-2/week-10/assessment/part_two_dao/src/main/java/com/techelevator/dao/JdbcBookDao.java
@@ -18,12 +18,26 @@ public class JdbcBookDao implements BookDao {
 
     @Override
     public List<Book> getBooks() {
-        return new ArrayList<>();
+        List<Book> getAllBooks = new ArrayList<>();
+        String sqlToGetAllBooks = "SELECT book_id, book_title, star_rating, out_of_print, foreword_by, publisher_id, published_date " +
+                "FROM book " ;
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sqlToGetAllBooks);
+        while (results.next()){
+            getAllBooks.add(mapRowToBook(results));
+        }
+        return getAllBooks;
     }
 
     @Override
     public Book createBook(Book newBook) {
-        return null;
+        String sqlToCreateBook = "INSERT INTO book ( book_title, star_rating, out_of_print, foreword_by, publisher_id, published_date)" +
+                "VALUES ( ?, ?, ?, ?, ?, ?)" +
+                "RETURNING book_id " ;
+        int bookId = jdbcTemplate.queryForObject(sqlToCreateBook,int.class,newBook.getBookTitle(),newBook.getStarRating(),newBook.isOutOfPrint(),newBook.getForewordBy(),newBook.getPublisherId(),newBook.getPublishedDate());
+
+        newBook.setBookId(bookId);
+
+        return newBook;
     }
 
     private Book mapRowToBook(SqlRowSet results) {
